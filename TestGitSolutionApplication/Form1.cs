3e833fc77feeb1ff20c06896d385ea7735c1bff8
ly281400407 <281400407@qq.com>
2015-07-20 06:49:34 +0800

    测试提交

--- TestGitSolutionApplication/Form1.cs
+++ TestGitSolutionApplication/Form1.cs
@@ -16,5 +16,10 @@ namespace TestGitSolutionApplication
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
